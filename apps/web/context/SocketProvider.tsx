a2acddeaea621acc97b5b333adf0761bcd16b50d
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-09-08 01:21:44 +0530

    implemented client side socket io

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React, { useCallback } from "react"
-
+import React, { useCallback, useEffect } from "react"
+import {io} from 'socket.io-client'
 interface SocketProviderProps {
     children?: React.ReactNode
 }
@@ -16,6 +16,13 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
     const sendMessage: ISocketContext["sendMessage"] = useCallback((msg) => {
         console.log("Send message", msg);
     }, [])
+
+    useEffect(()=> {
+        const _socket = io('http://localhost:8000')
+        return () => {
+            _socket.disconnect()
+        }
+    },[])
 
     return (
         <SocketContext.Provider value={{sendMessage}}>
