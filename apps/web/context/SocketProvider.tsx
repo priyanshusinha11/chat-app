63ecc7b783c3306dbb424a16be554911996fbcc1
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-09-08 01:26:01 +0530

    added custome hook

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React, { useCallback, useEffect } from "react"
-import {io} from 'socket.io-client'
+import React, { useCallback, useContext, useEffect } from "react"
+import { io } from 'socket.io-client'
 interface SocketProviderProps {
     children?: React.ReactNode
 }
@@ -11,21 +11,27 @@ interface ISocketContext {
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
+export const useSocket = () => {
+    const state = useContext(SocketContext);
+    if (!state) throw new Error(`state is undefined`)
+    return state;
+}
+
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
 
     const sendMessage: ISocketContext["sendMessage"] = useCallback((msg) => {
         console.log("Send message", msg);
     }, [])
 
-    useEffect(()=> {
+    useEffect(() => {
         const _socket = io('http://localhost:8000')
         return () => {
             _socket.disconnect()
         }
-    },[])
+    }, [])
 
     return (
-        <SocketContext.Provider value={{sendMessage}}>
+        <SocketContext.Provider value={{ sendMessage }}>
             {children}
         </SocketContext.Provider>
     )
